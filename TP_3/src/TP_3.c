ffdc18bde4c4f9f5de56d3fdc6511f87cedcf19d
fedesilveroutn <fedesilveromusico@gmail.com>
2021-11-05 23:11:27 -0300

    Creacion del directorio TP3

---
+++ TP_3/src/TP_3.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : TP_3.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout, NULL);
+
+	printf("Hola hola");
+	return EXIT_SUCCESS;
+}
